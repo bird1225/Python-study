a33071c03fff71e08f47cc6f8b09615ca834948d
汪凌峰 <540914848@qq.com>
2022-06-28 09:49:20 +0800

    [update]

--- InfluxDBUtil/InfluxdbUtil1.py
+++ InfluxDBUtil/InfluxdbUtil1.py
@@ -1,6 +1,8 @@
 # @Author  : 汪凌峰（Eric Wang）
 # @Date    : 2022/6/25
 import csv
+from logging import exception
+
 from influxdb import InfluxDBClient
 import os
 import json
@@ -44,8 +46,7 @@ def show_count(influxdb_client, measurement):
             count_file.write(measurement + ' ' + str(item[u'count_value']))
             count_file.write('\n')
 
-def show_databases(influxdb_client):
-    pass
+
 def create_database(influxdb_client, database):
     influxdb_client.create_database(database)
 
@@ -62,11 +63,89 @@ def get_points(influxdb_client, database, measurement):
     points = result.get_points()
     return points
 
+
+def arr_size(arr, size):
+    s = []
+    for i in range(0, int(len(arr)) + 1, size):
+        c = arr[i:i + size]
+        s.append(c)
+    newlist = [x for x in s if x]
+    return newlist
+
+
+mars_client.switch_database(WAVE_DATABASE)
 eric_client.switch_database(WAVE_DATABASE)
-eric_client2.switch_database('mars')
-result = eric_client.query('SELECT * FROM data_vib_wave_20')
-points = result.get_points()
-for item in points:
-    print(item)
+for i in range(0, 100):
+    measurement = 'data_vib_wave_' + str(i)
+    print(WAVE_DATABASE, '==>', measurement)
+    tag_keys = mars_client.query('SHOW tag keys FROM ' + measurement)
+    try:
+        for tag_key in tag_keys.raw['series'][0]['values']:
+            print(tag_key[0])
+        field_keys = mars_client.query('SHOW field keys FROM ' + measurement)
+
+        for field_key in field_keys.raw['series'][0]['values']:
+            print(field_key[0])
+    except Exception as e:
+        print(e)
+
+    result = mars_client.query('SELECT * FROM ' + measurement)
+    points = result.get_points()
+    json_body = []
+    for point in points:
+        # print(point)
+        try:
+            json_body.append(
+                {"measurement": measurement,  # 数据表的名称
+                 "tags": {
+                     "code": point['code'],
+                     "id": point['id']
+                 },
+                 "time": point['time'],
+                 "fields": {
+                     "DasCode": point['DasCode'],
+                     "IndexC": point['IndexC'],
+                     "IndexEven": point['IndexEven'],
+                     "IndexI": point['IndexI'],
+                     "IndexK": point['IndexK'],
+                     "IndexKur": point['IndexKur'],
+                     "IndexL": point['IndexL'],
+                     "IndexMax": point['IndexMax'],
+                     "IndexMean": point['IndexMean'],
+                     "IndexMin": point['IndexMin'],
+                     "IndexP": point['IndexP'],
+                     "IndexPP": point['IndexPP'],
+                     "IndexSK": point['IndexSK'],
+                     "IndexXr": point['IndexXr'],
+                     "RmsValue": point['RmsValue'],
+                     "TimeIndexTag": point['TimeIndexTag'],
+                     "WaveDataSrc": point['WaveDataSrc'],
+                     "alarm_level": point['alarm_level'],
+                     "convert_coef": point['convert_coef'],
+                     "data_section": point['data_section'],
+                     "desc": point['desc'],
+                     "isErrorSignal": point['isErrorSignal'],
+                     "quality": point['quality'],
+                     "rate": point['rate'],
+                     "rpm": point['rpm'],
+                     "src": point['src'],
+                     "station_code": point['station_code'],
+                     "unit": point['unit'],
+                     "value": point['value']
+                 }
+                 }
+            )
+        except Exception as e:
+            print(e)
+    arrs = arr_size(json_body, 20000)
+    for a in arrs:
+        eric_client.write_points(a)
+
+# eric_client.switch_database(WAVE_DATABASE)
+# eric_client2.switch_database('mars')
+# result = eric_client.query('SELECT * FROM data_vib_wave_20')
+# points = result.get_points()
+# for item in points:
+# print(item)
 # eric_client2.write_points(points)
 print("Game Over")
