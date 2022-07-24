2bf16987f39fa055b85bfefc644b82c4851cb2f1
汪凌峰 <540914848@qq.com>
2022-07-24 22:50:29 +0800

    [add]add redis file

--- RedisUtil/RedisUtil.py
+++ RedisUtil/RedisUtil.py
@@ -2,13 +2,15 @@
 # @Date    : 2022/7/21
 import redis
 
-pool = redis.ConnectionPool(host='localhost', port=6379, password='123456')
+master_host = '192.168.31.88'
+master_port = 6380
+password = 123456
+pool = redis.ConnectionPool(host=master_host, port=master_port, password=password)
 r = redis.Redis(connection_pool=pool)
-r.set('python:test:1', 123)
+r.set('python:test:4', 123)
 # r.flushall()
 value = r.get('python:test:1')
 if value is None:
     print('key is none')
 else:
     print(value)
-
